67a861f99eb12ffaae49b8ec459d61f8c09137ef
softee-p <zwhpena_4ever@hotmail.com>
2020-08-17 22:40:35 +0000

    Tested new function. updated and noted old version below. Now creates different lists of results for different devices.

--- testbed.py
+++ testbed.py
@@ -6,7 +6,36 @@ from my_tools import cmd_find_segments
 # Find usb adapters
 
 xa = cmd_find_segments("usb-devices", "Bus", "802.11")
+print(xa)
+keywords = ["Driver", "Manufacturer"]
+num_of_adapters = len(xa)
+print(num_of_adapters)
 
+results = []
+print(xa)
+for x in range(len(xa)):
+	temp_results = []
+	xa[x] = xa[x].split("\n")
+	for word in xa[x]:
+		for keyword in keywords:
+			if keyword in word:
+				temp_results.append(word[word.find(keyword) + len(keyword):])
+	if temp_results != []:
+		results.append(temp_results)
+print(results)
+
+
+
+
+
+
+
+
+
+
+"""
+xa = cmd_find_segments("usb-devices", "Bus", "802.11")
+print(xa)
 keyword = "Driver"
 num_of_adapters = len(xa)
 print(num_of_adapters)
@@ -14,9 +43,10 @@ print(num_of_adapters)
 new_results = []
 print(xa)
 for x in range(len(xa)):
-	xa[x] = xa[x].split("\n")
-	for word in xa[x]:
-		if keyword in word:
-			new_results.append(word[word.find(keyword) + len(keyword):])
+        xa[x] = xa[x].split("\n")
+        for word in xa[x]:
+                if keyword in word:
+                        new_results.append(word[word.find(keyword) + len(keyword):])
 
 print(new_results)
+"""
