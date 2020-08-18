fbc9cd8c49ab8dab1788a0acea7492c940224ca2
sotee-p <zwhpena_4ever@hotmail.com>
2020-08-18 03:14:55 +0300

    adjusted to spit out lists of words unique for every adapter

--- testbed.py
+++ testbed.py
@@ -10,12 +10,13 @@ keyword = "Driver"
 num_of_adapters = len(xa)
 print(num_of_adapters)
 
-new_results = []
+results = []
 print(xa)
 for x in range(len(xa)):
 	xa[x] = xa[x].split("\n")
+	temp_results = []
 	for word in xa[x]:
 		if keyword in word:
-			new_results.append(word[word.find(keyword) + len(keyword):])
-
-print(new_results)
+			temp_results.append(word[word.find(keyword) + len(keyword):])
+	results.append(temp_results)
+print(results)
