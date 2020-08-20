753ff27e6f2c3d9b60fc49d693f919b715ea7750
sotee-p <zwhpena_4ever@hotmail.com>
2020-08-20 19:16:16 +0300

    class method now creates instances

--- my_tools.py
+++ my_tools.py
@@ -4,14 +4,14 @@ import subprocess
 
 def cmd_grep(target):  # TODO: Update func
     grep_list = subprocess.run(['iw', 'dev'], capture_output=True, text=True)
-    if grep_list.returncode == 0:
+    if grep_list.returncode != 0:
         return print("Command failed. Exit code ", grep_list.returncode)
     interface_name0 = subprocess.run(['grep', target], stdout=subprocess.PIPE, text=True, input=grep_list.stdout)
     if interface_name0.stdout != "":
         return grep_list.stdout
 
 
-def cmd_find_lines(command, keyword=" "):
+def cmd_find_lines(command, keywords_list=""):
     # if keyword == False deactivate search.
 
     var1 = subprocess.Popen([command], universal_newlines=True,
@@ -24,17 +24,18 @@ def cmd_find_lines(command, keyword=" "):
 
     output = output.split("\n")
     output = output[0].split('\\')
-    if keyword == " ":
+    if keywords_list == "":
         return output
 
     output_list = []
-    for line in output:
-        if keyword in line and line not in output_list:
-            output_list.append(line)
+    for keyword in keywords_list:
+        for line in output:
+            if keyword in line and line not in output_list:
+                output_list.append(line)
     return output_list
 
 
-def cmd_find_words(command, keyword=" ", maxlen=0):     # Linux: ('ifconfig', "wlan", "mon", 5, 8)
+def cmd_find_words(command, keywords_list="", maxlen=0):     # Linux: ('ifconfig', "wlan", "mon", 5, 8)
     # find whole words starting with "keyword"
 
     var1 = subprocess.Popen([command], universal_newlines=True,
@@ -47,15 +48,16 @@ def cmd_find_words(command, keyword=" ", maxlen=0):     # Linux: ('ifconfig', "w
         return print("Subprocess-command failed. Exit code: ", var1.returncode, "Error: ", error)
 
     output_list = output.split()  # split string at every space
-    if keyword == " ":
+    if keywords_list == "":
         return output_list
     all_results = []
-    for word in output_list:
-        if keyword in word and word not in all_results:  # remove duplicates
-            if maxlen == 0:
-                all_results.append(word[word.find(keyword):])  # append word from start of keyword until the end
-            else:
-                all_results.append(word[word.find(keyword):word.find(keyword) + maxlen])
+    for keyword in keywords_list:
+        for word in output_list:
+            if keyword in word and word not in all_results:  # remove duplicates
+                if maxlen == 0:
+                    all_results.append(word[word.find(keyword):])  # append word from start of keyword until the end
+                else:
+                    all_results.append(word[word.find(keyword):word.find(keyword) + maxlen])
 
     if len(all_results) == 0:
         return print("No matching words found")
@@ -63,7 +65,7 @@ def cmd_find_words(command, keyword=" ", maxlen=0):     # Linux: ('ifconfig', "w
     return all_results
 
 
-def cmd_find_segments(command, split, keyword=""):
+def cmd_find_segments(command, split, keywords_list=""):
     # ('usb-devices', "Bus", "802.11")
     # split and include only words starting with key: cmd_find_segments("ls -la", "Jul", True, "22"))
 
@@ -77,38 +79,58 @@ def cmd_find_segments(command, split, keyword=""):
     output_split = output.split(split)
     # print(output)
     # print(output_split)
-    if keyword == "":
+    if keywords_list == "":
         return output_split
     results = []
-    for line in output_split:
-        if keyword in line:
-            results.append(line)
+    for keyword in keywords_list:
+        for line in output_split:
+            if keyword in line and line not in results:
+                results.append(line)
     # print(output_split)
     return results
 
 
-def cmd_find_values(command, segment_split, only_with, keywords_list):
+
+def cmd_find_values(command, data_list, split_point, exclusion_list="", inclusion_list=""):
+
     var1 = subprocess.Popen([command], universal_newlines=True,
                             stdout=subprocess.PIPE, shell=True,
                             stderr=subprocess.PIPE, )
     output, error = var1.communicate()
     if var1.returncode != 0:
         return print("Subprocess-command failed. Exit code: ", var1.returncode, "Error: ", error)
-    output_split = output.split(segment_split)
+    output_split = output.split(split_point)
 
+                        # If both ex and inc are passed,
+    segment_count = []  # returns number of segments for each
+                        # inclusion in order of inclusion_list.
     valid_segments = []
-    for segment in output_split:
-        if only_with in segment:
-            valid_segments.append(segment)
+    if exclusion_list != "":
+        for exclusion in exclusion_list:
+            for segment in output_split:
+                if exclusion not in segment and segment not in valid_segments:
+                    valid_segments.append(segment)
+        output_split = valid_segments
+        valid_segments = []
 
-    num_of_devices = len(valid_segments)
-    print(num_of_devices)
+    if inclusion_list != "":
+        for inclusion in inclusion_list:
+            temp_segment_count = 0
+            for segment in output_split:
+                if inclusion in segment and segment not in valid_segments:
+                    valid_segments.append(segment)
+                    temp_segment_count += 1
+            segment_count.append(temp_segment_count)
+    else:
+        valid_segments = output_split
+
+
     results = []
-
     for x in range(len(valid_segments)):
+        times_not_found = 0
         temp_results = []
         valid_segments[x] = valid_segments[x].split("\n")
-        for keyword in keywords_list:
+        for keyword in data_list:
             times_found = 0
             for sentence in valid_segments[x]:
                 sentence = sentence.split()
@@ -123,18 +145,20 @@ def cmd_find_values(command, segment_split, only_with, keywords_list):
                             temp_results.append(word[word.find(keyword) + len(keyword):])
             # print(times_found)
             if times_found == 0:
+                times_not_found += 1
                 temp_results.append("unknown")
-        if temp_results != []:
+        if times_not_found != data_list:
             results.append(temp_results)
-    return results
+
+    if not segment_count:
+        segment_count = len(results)
+
+    return results, segment_count
 
 
-
+# TODO: LINUX TEST2: not tested
 """
-TEST: OK!
-
-listog = ["admin", "poo"]
-var2 = cmd_find_values("ls -la", "\n", "admin", listog)
+cmd_find_values("usb-devices", Device._init_keywords, "Bus=", ["Driver=hub"], ["Product=802.11", "Driver="])
 
 print(var2)
 """
--- test_class.py
+++ test_class.py
@@ -1,62 +1,77 @@
-
+from my_tools import cmd_find_values
 
 
 class Device:
+    list_wireless = []
+    list_misc = []
 
-    def __init__(self, bus_device_num, version, version_id, product_id,
-                 manufacturer, device_type, serial, maxpwr, driver):
-        self.bus_device_num = bus_device_num
+    _init_keywords = ["Dev#=", "Ver=", "Vendor=", "ProdID=", "Manufacturer=",
+                      "Product=", "SerialNumber=", "MxPwr=", "Driver="]
+
+    def __init__(self, bus_id, version, version_id, product_id,
+                 manufacturer, product_type, serial, maxpwr, driver):
+        self.bus_id = bus_id
         self.version = version
         self.vendor_id = version_id
         self.product_id = product_id
         self.manufacturer = manufacturer
-        self.device_type = device_type
+        self.product_type = product_type
         self.serial = serial
         self.maxpwr = maxpwr
         self.driver = driver
 
+    @classmethod
+    def enumerate(cls):
+        Device.list_wireless = []
+        Device.list_misc = []
+        re = cmd_find_values("usb-devices", Device._init_keywords, "Bus=",
+                             ["Driver=hub"], ["Product=802.11", "Driver="])
+        devices = re[0]
+        # devices_count = re[1]
+        for item in devices:
+            if "802.11" in item[5]:
+                Device.list_wireless.append(Wireless(item[0], item[1], item[2], item[3], item[4],
+                                                     item[5], item[6], item[7], item[8]))
+            else:
+                Device.list_misc.append(Device(item[0], item[1], item[2], item[3], item[4],
+                                               item[5], item[6], item[7], item[8]))
 
-class WirelessAdapter(Device):
 
-    def __init__(self, bus_device_num, version, version_id, product_id,
-                 manufacturer, device_type, serial, maxpwr, driver):
-        super().__init__(bus_device_num, version, version_id, product_id,
-                         manufacturer, device_type, serial, maxpwr, driver)
+class Wireless(Device):
+
+    def __init__(self, bus_id, version, version_id, product_id,
+                 manufacturer, product_type, serial, maxpwr, driver):
+        super().__init__(bus_id, version, version_id, product_id,
+                         manufacturer, product_type, serial, maxpwr, driver)
 
         self.interfaces = []
         self.mac = {}
-
 
     def add_interface(self, interface):
         self.interfaces.append(interface)
 
 
-
 class Interface:
 
-    def __init__(self, iface_id="none"):
+    def __init__(self, iface_id, serial, driver):
         self.iface_id = iface_id
+        self.serial = serial
+        self.driver = driver
         self.isup = False
         self.mode = {}
         self.test = False
 
+    @classmethod
+    def create_interface(cls, iface_id, serial, driver):
+        return Interface(iface_id, serial, driver)
 
-# TODO: Crete class instances from results
 
-# wlan_list = Interface.enumerate()[0]
-# print(wlan_list)
-# mon_list = Interface.enumerate()[1]
-# print(wlan_list)
+# interface_1 = Interface.create_interface("wlan0")
+# print(interface_1.iface_id)
+
+
+# TODO 1: Crete class instances from results
 '''
-usb1 = classes_list[0]
+Comments:_
 
-print(usb1.present())
-
-
-adapter0 = ManagedInterface(1102, 2466)
-print(adapter0.bus_device)
-adapter0.present()
-adapter0.bus_device = "new"
-print(adapter0.bus_device)
-print(adapter0.interface_name)
 '''
